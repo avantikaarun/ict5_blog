530bb757388be615fd40075ff7bfd7f6c1a4c242
AVANTIKA ARUN <avantika@AVANTIKAs-MacBook-Air.local>
2023-05-27 16:26:52 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import { useForm } from 'react-hook-form';
-import Form from './Form.jsx';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Col from 'react-bootstrap/Col';
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import { useForm } from "react-hook-form";
+import Form from "./Form.jsx";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Table from "react-bootstrap/Table";
 
 export default function App() {
   const { register, handleSubmit } = useForm();
   const [blog, setBlog] = useState([]);
 
   const getData = async () => {
-    const data = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await fetch("https://jsonplaceholder.typicode.com/posts");
     const jsonData = await data.json();
     setBlog(jsonData);
   };
@@ -35,26 +35,24 @@ export default function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      <Form></Form>
       <Container>
-        <Form>
-
-        </Form>
-        <br />
-
         <Row>
-          <br />
-          <h1 className="text-center mb-3">BLOGS</h1>
-          <Card style={{ width: '23rem' }}>
+          
+          <h1 className="text-center mb-3">Dashboard</h1>
+          <Card style={{ width: "23rem" }}>
             <Card.Img
               variant="top"
               src="https://raisingsimple.com/wp-content/uploads/2019/11/anita-austvika-zEKvzPonptU-unsplash-400x600.jpg"
             />
             <Card.Body>
-              <Card.Title>15 THINGS YOU NEED TO HEAR FOR A SIMPLE HOLIDAY</Card.Title>
+              <Card.Title>
+                15 THINGS YOU NEED TO HEAR FOR A SIMPLE HOLIDAY
+              </Card.Title>
               <Button variant="primary">VISIT</Button>
             </Card.Body>
           </Card>
-          <Card style={{ width: '23rem' }}>
+          <Card style={{ width: "23rem" }}>
             <Card.Img
               variant="top"
               src="https://raisingsimple.com/wp-content/uploads/2019/10/IMG_0039-400x600.jpg"
@@ -64,7 +62,7 @@ export default function App() {
               <Button variant="primary">VISIT</Button>
             </Card.Body>
           </Card>
-          <Card style={{ width: '23rem' }}>
+          <Card style={{ width: "23rem" }}>
             <Card.Img
               variant="top"
               src="https://raisingsimple.com/wp-content/uploads/2019/04/sofy-m-halftoneco-1388841-unsplash-400x600.jpg"
@@ -74,7 +72,7 @@ export default function App() {
               <Button variant="primary">VISIT</Button>
             </Card.Body>
           </Card>
-          <Card style={{ width: '23rem' }}>
+          <Card style={{ width: "23rem" }}>
             <Card.Img
               variant="top"
               src="https://raisingsimple.com/wp-content/uploads/2020/09/mb1-2-400x600.jpg"
@@ -84,9 +82,36 @@ export default function App() {
               <Button variant="primary">VISIT</Button>
             </Card.Body>
           </Card>
-          
-          
+          <Card style={{ width: "23rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://raisingsimple.com/wp-content/uploads/2021/08/D6991311-C92C-40A6-84E5-147C7675F67A-400x600.jpeg"
+            />
+            <Card.Body>
+              <Card.Title>MINIMALIST CLOSET TOUR</Card.Title>
+              <Button variant="primary">VISIT</Button>
+            </Card.Body>
+          </Card>
         </Row>
+        <h1>Data</h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {blog.map((item, index) => (
+              <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.body}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </Container>
     </div>
   );
