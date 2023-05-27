a98446024c2dd6b8e6efda9a28c064a45a54511f
AVANTIKA ARUN <avantika@AVANTIKAs-MacBook-Air.local>
2023-05-27 19:51:19 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,118 +1,18 @@
-import React, { useEffect, useState } from "react";
-import Container from "react-bootstrap/Container";
-import { useForm } from "react-hook-form";
-import Form from "./Form.jsx";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Table from "react-bootstrap/Table";
+import React from 'react'
+import Nav1 from './units/Nav1';
+import Home from './pages/Home';
+import AddBlog from './pages/Addblog'
+import { Routes, Route } from "react-router-dom"
+
 
 export default function App() {
-  const { register, handleSubmit } = useForm();
-  const [blog, setBlog] = useState([]);
-
-  const getData = async () => {
-    const data = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const jsonData = await data.json();
-    setBlog(jsonData);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home"></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="# add blog">Addblog</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Form></Form>
-      <Container>
-        <Row>
-          
-          <h1 className="text-center mb-3">Dashboard</h1>
-          <Card style={{ width: "23rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://raisingsimple.com/wp-content/uploads/2019/11/anita-austvika-zEKvzPonptU-unsplash-400x600.jpg"
-            />
-            <Card.Body>
-              <Card.Title>
-                15 THINGS YOU NEED TO HEAR FOR A SIMPLE HOLIDAY
-              </Card.Title>
-              <Button variant="primary">VISIT</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "23rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://raisingsimple.com/wp-content/uploads/2019/10/IMG_0039-400x600.jpg"
-            />
-            <Card.Body>
-              <Card.Title>MOVING A HOUSE</Card.Title>
-              <Button variant="primary">VISIT</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "23rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://raisingsimple.com/wp-content/uploads/2019/04/sofy-m-halftoneco-1388841-unsplash-400x600.jpg"
-            />
-            <Card.Body>
-              <Card.Title>IT'S BACKED BY SCIENCE.LET CLUTTER GO</Card.Title>
-              <Button variant="primary">VISIT</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "23rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://raisingsimple.com/wp-content/uploads/2020/09/mb1-2-400x600.jpg"
-            />
-            <Card.Body>
-              <Card.Title>MODERN MASTER BATHROOM</Card.Title>
-              <Button variant="primary">VISIT</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "23rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://raisingsimple.com/wp-content/uploads/2021/08/D6991311-C92C-40A6-84E5-147C7675F67A-400x600.jpeg"
-            />
-            <Card.Body>
-              <Card.Title>MINIMALIST CLOSET TOUR</Card.Title>
-              <Button variant="primary">VISIT</Button>
-            </Card.Body>
-          </Card>
-        </Row>
-        <h1>Data</h1>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Title</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-          <tbody>
-            {blog.map((item, index) => (
-              <tr key={index}>
-                <td>{item.id}</td>
-                <td>{item.title}</td>
-                <td>{item.body}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Container>
+      <Nav1></Nav1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AddBlog.jsx" element={<AddBlog />} />
+      </Routes>
     </div>
-  );
+  )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react'
+import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 
 export default function Home() {
+
+  const [blog, setBlog] = useState([]);
+
+
+
+
+  const getData = async () => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const jsonData = await data.json();
+    setBlog(jsonData)
+  }
+
+
+
+
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+
+
+
   return (
-    <div>Home</div>
+    <div>
+      <Container>
+
+
+
+        <Row>
+          {
+            blog.map((item, index) => {
+              return (
+                <Col key={index}>
+                  <Card className='mt-3' style={{ width: '20rem', maxHeight: '100rem', height: '33rem' }}>
+                  <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8o3h9bYVLymjUr9OM-g0gJXpcGODbgVV1NJSygsivf4H5fgVxW_6Ntv-fzxW3rgtBVLU&usqp=CAU" />
+                    <Card.Body>
+                      <Card.Title>{item.title}</Card.Title>
+                      <Card.Text>{item.body}</Card.Text>
+                      <Button variant="dark">Read More</Button>
+                      
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )
+            })
+          }
+        </Row>
+      </Container>
+    </div>
   )
 }
