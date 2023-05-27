72ad71c42002297e855875456ebc1d44bf611ee4
AVANTIKA ARUN <avantika@AVANTIKAs-MacBook-Air.local>
2023-05-27 19:52:47 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import Nav1 from './units/Nav1';
-import Home from './pages/Home';
-import AddBlog from './pages/Addblog'
-import { Routes, Route } from "react-router-dom"
-
+import React from "react";
+import Nav1 from "./units/Nav1";
+import Home from "./pages/Home";
+import AddBlog from "./pages/Addblog";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
@@ -14,5 +13,5 @@ export default function App() {
         <Route path="/AddBlog.jsx" element={<AddBlog />} />
       </Routes>
     </div>
-  )
+  );
 }
